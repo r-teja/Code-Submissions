bc171101c222c6c683bf2663fa43a53fea51aa2b
Ravi Teja <66872471+r-teja@users.noreply.github.com>
2022-11-04 22:02:36 +0530

    Added solution to Maximum Score From Removing Stones

---
+++ Ravi-Teja/MaximumScoreFromRemovingStones.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int> pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        int ans=0;
+        while(pq.size() > 1){
+            int x=pq.top();
+            pq.pop();
+            int y=pq.top();
+            pq.pop();
+            if(x-1 > 0){
+                pq.push(x-1);
+            }
+            if(y-1 > 0){
+                pq.push(y-1);
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
